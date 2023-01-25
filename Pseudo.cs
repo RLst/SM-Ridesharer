82f3243eedfe23de8b3a559c5a737aa3518fc3d6
Tony Le <letony@icloud.com>
2023-01-25 19:50:32 +0700

    Add score controller; Add new graphics - shipper, numbers, road and lines

--- Pseudo.cs
+++ Pseudo.cs
@@ -1,149 +1,149 @@
-Rideshare Pseudocode
-----------------
+//Rideshare Pseudocode
+//----------------
 
-Making obstacles move
-
-Vec2 horizonPoint;
-Vec2 horizonPlane;
-Vec2[] spawnPoints;
-Vec2[] scenicSpawnPoints;
-float spawnDuration = 2;
-
-bool spawn1 = false;
-bool spawn2 = false;
-bool spawn3 = false;
-bool spawn4 = false;
-//etc
-
-float spawnDuration = 1f;
-
-bool isGameStarted = false;
-bool isPlaying = false;
-
-void Update()
+public class RideSharer
 {
-	HandleObstacleSpawning();
-	HandleScenicObjectSpawning();
+	Vec2 horizonPoint;
+	Vec2 horizonPlane;
+	Vec2[] spawnPoints;
+	Vec2[] scenicSpawnPoints;
+	float spawnDuration = 2;
 
-	HandleObjectMovement();
+	bool spawn1 = false;
+	bool spawn2 = false;
+	bool spawn3 = false;
+	bool spawn4 = false;
+	//etc
 
-	HandleObjectDespawn();
-}
+	float spawnDuration = 1f;
 
-void HandleObstacleSpawning()
-{
-	const chance = 4;
-	if (random(10) > chance)
-		return;
+	bool isGameStarted = false;
+	bool isPlaying = false;
 
-	Vec2 spawnPoint = spawnPoints[random[4]];
-
-	foreach (var x in obstacles)
+	void Update()
 	{
-		if (GetVisibilty(x) == false)
+		HandleObstacleSpawning();
+		HandleScenicObjectSpawning();
+
+		HandleObjectMovement();
+
+		HandleObjectDespawn();
+	}
+
+	void HandleObstacleSpawning()
+	{
+		const int chance = 4;
+		if (random(10) > chance)
+			return;
+
+		Vec2 spawnPoint = spawnPoints[random[4]];
+
+		foreach (var x in obstacles)
 		{
-			SetVisibility(x, true);
-			x.position.TransitByTime(from: spawnPoint, to: endPos, duration: spawnDuration, interpolation: Interpolation.EaseIn, pingpong: false);
+			if (GetVisibilty(x) == false)
+			{
+				SetVisibility(x, true);
+				x.position.TransitByTime(from: spawnPoint, to: endPos, duration: spawnDuration, interpolation: Interpolation.EaseIn, pingpong: false);
+			}
 		}
 	}
-}
 
-void HandleScenicObjectSpawning(Vec2 startPos)
-{
-	const chance = 3;
-	if (random(10) > chance)
-		return;
-
-	Vec2 spawnPoint = scenicSpawnPoints[random[2]];
-
-	foreach (var x in scenicObjects)
+	void HandleScenicObjectSpawning(Vec2 startPos)
 	{
-		if (GetVisibility(x) == false)
+		const int chance = 3;
+		if (random(10) > chance)
+			return;
+
+		Vec2 spawnPoint = scenicSpawnPoints[random[2]];
+
+		foreach (var x in scenicObjects)
 		{
-			SetVisibility(x, true);
-			x.position = startPos;
+			if (GetVisibility(x) == false)
+			{
+				SetVisibility(x, true);
+				x.position = startPos;
+			}
 		}
 	}
-}
 
-// void HandleObjectMovement()
-// {
-// 	//Move towards the
-// 	foreach (var x in obstacles)
-// 	{
-// 		if (GetVisibility(o) == false)
-// 			return;
+	// void HandleObjectMovement()
+	// {
+	// 	//Move towards the
+	// 	foreach (var x in obstacles)
+	// 	{
+	// 		if (GetVisibility(o) == false)
+	// 			return;
 
-// 		x.positi
-// 	}
-// }
+	// 		x.positi
+	// 	}
+	// }
 
-/// <summary>
-/// Check if objects
-/// </summary>
-void HandleObjectDespawn()
-{
-	foreach (var o in obstacles)
+	/// <summary>
+	/// Check if objects
+	/// </summary>
+	void HandleObjectDespawn()
 	{
-		if (GetVisibility(o) == true && o.position > horizonPlane.y)
+		foreach (var o in obstacles)
 		{
-			SetVisibility(o, false);
+			if (GetVisibility(o) == true && o.position > horizonPlane.y)
+			{
+				SetVisibility(o, false);
+			}
 		}
 	}
-}
 
 
+	//GAME STATES
+	//Game ready
+	// 	isPlaying = false;
+	// isGameStarted = false;
 
-//GAME STATES
-//Game ready
-isPlaying = false;
-isGameStarted = false;
+	// //Game start
+	// isPlaying = true;
+	// isGameStarted = true;
 
-//Game start
-isPlaying = true
-isGameStarted = true;
+	// //Game end
+	// isPlaying = false;
+	// isGameStarted = true;
 
-//Game end
-isPlaying = false;
-isGameStarted = true;
+	// //Game reset
+	// isPlaying = false;
+	// isGameStarted = false;
 
-//Game reset
-isPlaying = false;
-isGameStarted = false;
-
-/// Gameloop
-float timeSinceLastRespawn = 0;
+	/// Gameloop
+	float timeSinceLastRespawn = 0;
 
 
-void Start()
-{
-	ResetGame();
-}
+	void Start()
+	{
+		ResetGame();
+	}
 
-void ResetGame()
-{
-	gameSpeed = 0;
-	timeSinceLastRespawn = 0;
-	isPlaying = false;
-	isGameStarted = false;
-}
+	void ResetGame()
+	{
+		gameSpeed = 0;
+		timeSinceLastRespawn = 0;
+		isPlaying = false;
+		isGameStarted = false;
+	}
 
-public void StartGame()
-{
-	isPlaying = true;
-	isGameStarted = true;
-}
+	public void StartGame()
+	{
+		isPlaying = true;
+		isGameStarted = true;
+	}
 
-void Update()
-{
-	if (!isPlaying)
-		return;
+	void Update()
+	{
+		if (!isPlaying)
+			return;
 
-	
-}
 
-public void EndGame()
-{
-	ShowDeathSequence();
-	ShowScore();
+	}
+
+	public void EndGame()
+	{
+		ShowDeathSequence();
+		ShowScore();
+	}
 }
